f19bff8bb2a5aa419a92989195ce07c931d7fd36
Darking <zsamuel737@gmail.com>
2024-03-18 15:04:45 +0100

    fix

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -47,7 +47,7 @@
                     @foreach ($content as $item)
                     <tr class="bg-gray-100  even:dark:bg-gray-800 border-b dark:border-gray-700">
                       <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
-                        <img src={{asset("images/activiste/eco3.jpg")}} alt="" class="object-cover w-full h-full">
+                        <img src={{asset("images/activiste/eco3.jpg")}} alt="" class="object-cover w-full h-40">
                       </th>
                       <td class="px-6 py-4 text-justify">
                         {{ $item->name->description }}
